e92da83fc9f14f5845fafa2d14503c91a66677f9
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-31 17:00:25 +0530

    feat: getting events on login

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import { GoogleLogin } from 'react-google-login';
 import { gapi } from 'gapi-script';
 
+const CLIENT_ID =
+  '986741280832-e719osmsgs3ck3csktejng81onkdvdd0.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyAdgT4JxHeZe_Wf7LeCannvfE4HngQNFJI';
+
+const DISCOVERY_DOC =
+  'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
 function App() {
-  const clientId =
-    '986741280832-e719osmsgs3ck3csktejng81onkdvdd0.apps.googleusercontent.com';
+  const [suc, setSuc] = useState(false);
 
   useEffect(() => {
     const initClient = () => {
       gapi.client.init({
-        clientId: clientId,
+        clientId: CLIENT_ID,
         scope:
           'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar',
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
       });
     };
     gapi.load('client:auth2', initClient);
   });
 
+  useEffect(() => {
+    if (suc) {
+      (async () => {
+        const request = {
+          calendarId: 'primary',
+          timeMin: new Date().toISOString(),
+          showDeleted: false,
+          singleEvents: true,
+          maxResults: 10,
+          orderBy: 'startTime',
+        };
+
+        let response = await gapi.client?.calendar?.events.list(request);
+
+        console.log({ response });
+      })();
+    }
+  }, [suc]);
+
   const onSuccess = (res) => {
     console.log('success:', res);
+
+    setSuc(true);
   };
   const onFailure = (err) => {
     console.log('failed:', err);
@@ -30,7 +61,7 @@ function App() {
   return (
     <div className='App'>
       <GoogleLogin
-        clientId={clientId}
+        clientId={CLIENT_ID}
         buttonText='Sign in with Google'
         onSuccess={onSuccess}
         onFailure={onFailure}
