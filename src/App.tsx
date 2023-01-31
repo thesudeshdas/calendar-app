6842b9ac5838212f51575a976cda1de45e5e7840
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-31 16:45:11 +0530

    feat: google login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
 function App() {
+  const clientId =
+    '986741280832-e719osmsgs3ck3csktejng81onkdvdd0.apps.googleusercontent.com';
+
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope:
+          'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar',
+      });
+    };
+    gapi.load('client:auth2', initClient);
+  });
+
+  const onSuccess = (res) => {
+    console.log('success:', res);
+  };
+  const onFailure = (err) => {
+    console.log('failed:', err);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText='Sign in with Google'
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+      />
+
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId='986741280832-e719osmsgs3ck3csktejng81onkdvdd0.apps.googleusercontent.com'>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
